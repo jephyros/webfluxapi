f536e5d34ce62fcfb2067eb83abe4f97496a22c6
CIS-Mac_BW <kr.chis@gmail.com>
2020-10-19 16:31:36 +0900

    build.gradle 수정

--- src/main/java/kr/chis/webfluxapi/book/domain/Book.java
+++ src/main/java/kr/chis/webfluxapi/book/domain/Book.java
@@ -1,15 +1,17 @@
 package kr.chis.webfluxapi.book.domain;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "book")
 @Getter
 @Setter
 @AllArgsConstructor
 public class Book {
     private Long id;
     private String bookName;
+
 
 }
